f2ccc2d1d6f58d8f6162f2596bcbba7e275df53c
Rob Landley <rob@landley.net>
2007-04-30 00:08:48 -0400

    Add true and false.

---
+++ toys/false.c
@@ -0,0 +1,11 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * false.c - Return nonzero.
+ */
+
+#include "toys.h"
+
+int false_main(void)
+{
+	return 1;
+}
---
+++ toys/true.c
@@ -0,0 +1,11 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * true.c - Return zero.
+ */
+
+#include "toys.h"
+
+int true_main(void)
+{
+	return 0;
+}
