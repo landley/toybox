56dac2a9ca1625af5343957a19686bceab277129
Rob Landley <rob@landley.net>
2007-06-18 00:17:04 -0400

    Add a dummy "touch" to make it all build again.

---
+++ toys/touch.c
@@ -0,0 +1,14 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * touch.c - Modify a file's timestamps (or length).
+ */
+
+#include "toys.h"
+
+# warning touch unimplemented
+
+int touch_main(void)
+{
+	printf("Hello world\n");
+	return 0;
+}
