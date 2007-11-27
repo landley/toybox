b938705155af45d3cc2cd6c8583e496795e79c0d
Rob Landley <rob@landley.net>
2007-11-27 01:19:40 -0600

    Add comment and very minor tweak.

--- toys/dirname.c
+++ toys/dirname.c
@@ -1,8 +1,14 @@
+/* vi: set sw=4 ts=4: */
+/* dirname.c - print directory portion of path, or "." if none.
+ *
+ * See http://www.opengroup.org/onlinepubs/009695399/utilities/dirname.html
+ */
+
 #include "toys.h"
 #include <libgen.h>
 
 int dirname_main(void)
 {
-	puts(dirname(toys.optargs[0]));
+	puts(dirname(*toys.optargs));
 	return 0;
 }
