0d88c38881088f0c8ee7a09dbba01821253a778c
Rob Landley <rob@landley.net>
2007-11-27 01:36:50 -0600

    Add comment, and whitespace changes.

--- toys/tty.c
+++ toys/tty.c
@@ -1,13 +1,17 @@
+/* vi: set sw=4 ts=4: */
+/* tty.c - print terminal name of stdin
+ *
+ * See http://www.opengroup.org/onlinepubs/009695399/utilities/tty.html
+ */
+
 #include "toys.h"
 
 int tty_main(void)
 {
 	char *name = ttyname(0);
 	if (!toys.optflags) {
-		if (name)
-			puts(name);
-		else
-			puts("Not a tty");
+		if (name) puts(name);
+		else puts("Not a tty");
 	}
 	return !name;
 }
