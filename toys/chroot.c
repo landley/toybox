ebff8ee282dd2eee7c2fd9da0587573520b41df0
Rob Landley <rob@landley.net>
2007-12-03 20:05:14 -0600

    Other chroots fall back to "/bin/sh -i", so add the -i.

--- toys/chroot.c
+++ toys/chroot.c
@@ -1,13 +1,15 @@
 /* vi: set sw=4 ts=4: */
 /*
  * chroot.c - Run command in new root directory.
+ *
+ * Not in SUSv3.
  */
 
 #include "toys.h"
 
 void chroot_main(void)
 {
-	char *binsh[] = {"/bin/sh", 0};
+	char *binsh[] = {"/bin/sh", "-i", 0};
 	if (chdir(*toys.optargs) || chroot("."))
 		perror_exit("%s", *toys.optargs);
 	xexec(toys.optargs[1] ? toys.optargs+1 : binsh);
