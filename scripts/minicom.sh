3e09c88b0db4d5a0dad64c24448b437c0045c145
Rob Landley <rob@landley.net>
2007-12-16 00:04:17 -0600

    Script to use stty with netcat -f to talk to a serial port.

---
+++ scripts/minicom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# If you want to use toybox netcat to talk to a serial port, use this.
+
+if [ ! -f "$1" ]
+then
+  echo "Usage: minicom.sh /dev/ttyS0"
+  exit 1
+fi
+
+stty 115200 -F "$1"
+stty raw -echo -ctlecho -F "$1"
+stty raw -echo  # Need to do it on stdin, too.
+./toybox netcat -f "$1"
+stty cooked echo  # Put stdin back.
