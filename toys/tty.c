9e061712b8098eae9d75b7ff9c6d445f8454ab94
Rob Landley <rob@landley.net>
2011-11-15 00:57:45 -0600

    Implement tty command.

---
+++ toys/tty.c
@@ -0,0 +1,32 @@
+/* vi: set sw=4 ts=4:
+ *
+ * tty.c - Show stdin's terminal name
+ *
+ * Copyright 2011 Rob Landley <rob@landley.net>
+ *
+ * See http://opengroup.org/onlinepubs/9699919799/utilities/tty.html
+
+USE_TTY(NEWTOY(tty, "s", TOYFLAG_USR|TOYFLAG_BIN))
+
+config TTY
+	bool "tty"
+	default y
+	help
+	  Show filename of terminal connected to stdin.
+
+	  Prints "not a tty" and exits with nonzero status if no terminal
+	  is connected to stdin.
+
+	  -s	silent mode
+*/
+
+#include "toys.h"
+
+void tty_main(void)
+{
+	char *tty = ttyname(0);
+
+	if (!toys.optflags) puts(tty ? tty : "not a tty");
+
+	toys.exitval = !tty;
+}
