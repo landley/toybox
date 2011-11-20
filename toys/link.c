c157b4cc656aa938961e03580eec4cfcfc0717a3
Rob Landley <rob@landley.net>
2011-11-20 11:39:13 -0600

    Add link.

---
+++ toys/link.c
@@ -0,0 +1,27 @@
+/* vi: set sw=4 ts=4:
+ *
+ * link.c - hardlink a file
+ *
+ * Copyright 2011 Rob Landley <rob@landley.net>
+ *
+ * See http://opengroup.org/onlinepubs/9699919799/utilities/link.html
+
+USE_LINK(NEWTOY(link, "<2>2", TOYFLAG_USR|TOYFLAG_BIN))
+
+config LINK
+	bool "link"
+	default y
+	help
+	  usage: link FILE NEWLINK
+
+	  Create hardlink to a file.
+*/
+
+#include "toys.h"
+
+void link_main(void)
+{
+	if (link(toys.optargs[0], toys.optargs[1]))
+		perror_exit("couldn't link '%s' to '%s'", toys.optargs[0],
+			toys.optargs[1]);
+}
