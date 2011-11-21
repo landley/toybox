624856d516c7a77bdf3bf853ccc2997a6f78df27
Rob Landley <rob@landley.net>
2011-11-20 21:36:37 -0600

    Implement unlink.

---
+++ toys/unlink.c
@@ -0,0 +1,26 @@
+/* vi: set sw=4 ts=4:
+ *
+ * unlink.c - delete one file
+ *
+ * Copyright 2011 Rob Landley <rob@landley.net>
+ *
+ * See http://opengroup.org/onlinepubs/9699919799/utilities/unlink.html
+
+USE_UNLINK(NEWTOY(unlink, "<1>1", TOYFLAG_USR|TOYFLAG_BIN))
+
+config UNLINK
+	bool "unlink"
+	default y
+	help
+	  usage: unlink FILE
+
+	  Deletes one file.
+*/
+
+#include "toys.h"
+
+void unlink_main(void)
+{
+	if (unlink(*toys.optargs))
+		perror_exit("Couldn't unlink `%s'", *toys.optargs);
+}
