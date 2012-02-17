1aa66ba2084a7e1ad7b0a5bbece58a835c749096
Rob Landley <rob@landley.net>
2012-02-17 12:06:12 -0600

    Add V=1 support to make.

--- scripts/make.sh
+++ scripts/make.sh
@@ -96,9 +96,15 @@ TOYFILES=$(cat .config | sed -nre 's/^CONFIG_(.*)=y/\1/;t skip;b;:skip;s/_.*//;p
 
 echo "Compile toybox..."
 
-$DEBUG $CC $CFLAGS -I . -o toybox_unstripped $OPTIMIZE main.c lib/*.c \
+do_loudly()
+{
+  [ ! -z "$V" ] && echo "$@"
+  "$@"
+}
+
+do_loudly $CC $CFLAGS -I . -o toybox_unstripped $OPTIMIZE main.c lib/*.c \
   $TOYFILES -Wl,--as-needed,-lutil,--no-as-needed || exit 1
-$DEBUG $STRIP toybox_unstripped -o toybox || exit 1
+do_loudly $STRIP toybox_unstripped -o toybox || exit 1
 # gcc 4.4's strip command is buggy, and doesn't set the executable bit on
 # its output the way SUSv4 suggests it do so.
-$DEBUG chmod +x toybox || exit 1
+do_loudly chmod +x toybox || exit 1
