344c267e9fe5c6a39efdbf0305bd873d5bef65f3
Rob Landley <rob@landley.net>
2012-03-03 10:56:11 -0600

    Fix from Georgi Chorbadzhiyski to make cross compiling more robust.

--- scripts/genconfig.sh
+++ scripts/genconfig.sh
@@ -13,7 +13,7 @@ probeconfig()
   # Probe for container support on target
 
   echo -e "# container support\nconfig TOYBOX_CONTAINER\n\tbool" || return 1
-  $CC -c -xc -o /dev/null - << EOF
+  ${CROSS_COMPILE}${CC} -c -xc -o /dev/null - 2>/dev/null << EOF
     #include <sched.h>
     int x=CLONE_NEWNS|CLONE_NEWUTS|CLONE_NEWIPC|CLONE_NEWNET;
 EOF
--- scripts/make.sh
+++ scripts/make.sh
@@ -102,9 +102,9 @@ do_loudly()
   "$@"
 }
 
-do_loudly $CC $CFLAGS -I . -o toybox_unstripped $OPTIMIZE main.c lib/*.c \
-  $TOYFILES -Wl,--as-needed,-lutil,--no-as-needed || exit 1
-do_loudly $STRIP toybox_unstripped -o toybox || exit 1
+do_loudly ${CROSS_COMPILE}${CC} $CFLAGS -I . -o toybox_unstripped $OPTIMIZE \
+  main.c lib/*.c $TOYFILES -Wl,--as-needed,-lutil,--no-as-needed || exit 1
+do_loudly ${CROSS_COMPILE}${STRIP} toybox_unstripped -o toybox || exit 1
 # gcc 4.4's strip command is buggy, and doesn't set the executable bit on
 # its output the way SUSv4 suggests it do so.
 do_loudly chmod +x toybox || exit 1
