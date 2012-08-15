c96444da80b7db3bd5d7c3e05e15f973840378c6
Elie De Brauwer <eliedebrauwer@gmail.com>
2012-08-15 12:53:54 +0200

    Adding usleep

---
+++ toys/usleep.c
@@ -0,0 +1,32 @@
+/* vi: set sw=4 ts=4:
+ *
+ * usleep.c - Wait for a number of microseconds.
+ *
+ * Copyright 2012 Elie De Brauwer <eliedebrauwer@gmail.com>
+ *
+ * No standard.
+
+USE_USLEEP(NEWTOY(usleep, "<1", TOYFLAG_BIN))
+
+config USLEEP
+	bool "usleep"
+	default y
+	help
+	  usage: usleep MICROSECONDS
+
+	  Pause for MICROSECONDS microseconds.
+
+*/
+
+#include "toys.h"
+
+void usleep_main(void)
+{
+    struct timespec tv;
+    long delay = atol(*toys.optargs);
+    
+    tv.tv_sec = delay/1000000;
+    tv.tv_nsec = (delay%1000000) * 1000;
+    toys.exitval = !!nanosleep(&tv, NULL);
+
+}
