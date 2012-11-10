ffba380a466e010e475462bdfacf0a7045f9e00b
Rob Landley <rob@landley.net>
2012-11-10 14:34:01 -0600

    Fix reversed test: patch creates a file if /dev/null or dated the epoch _and_ first hunk being replaced is 0 lines at start of file, not one or the other.

--- toys/posix/patch.c
+++ toys/posix/patch.c
@@ -387,7 +387,7 @@ void patch_main(void)
 				// If we've got a file to open, do so.
 				} else if (!(toys.optflags & FLAG_p) || i <= TT.prefix) {
 					// If the old file was null, we're creating a new one.
-					if (!strcmp(oldname, "/dev/null") || !oldsum) {
+					if (!strcmp(oldname, "/dev/null") && !oldsum) {
 						printf("creating %s\n", name);
 						s = strrchr(name, '/');
 						if (s) {
