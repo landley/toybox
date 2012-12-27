d0d0f40bbbc43896e071702f51e09f206ad1809e
Felix Janda <felix.janda@posteo.de>
2012-12-27 17:52:14 +0100

    mkfifo -m is already implemented.

--- toys/posix/mkfifo.c
+++ toys/posix/mkfifo.c
@@ -3,8 +3,6 @@
  * Copyright 2012 Georgi Chorbadzhiyski <georgi@unixsol.org>
  *
  * See http://opengroup.org/onlinepubs/9699919799/utilities/mkfifo.html
- *
- * TODO: Add -m
 
 USE_MKFIFO(NEWTOY(mkfifo, "<1m:", TOYFLAG_BIN))
 
@@ -30,9 +28,7 @@ void mkfifo_main(void)
   char **s;
 
   TT.mode = 0666;
-  if (toys.optflags & FLAG_m) {
-    TT.mode = string_to_mode(TT.m_string, 0);
-  }
+  if (toys.optflags & FLAG_m) TT.mode = string_to_mode(TT.m_string, 0);
 
   for (s = toys.optargs; *s; s++) {
     if (mknod(*s, S_IFIFO | TT.mode, 0) < 0) {
