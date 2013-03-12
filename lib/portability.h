25b043bff9de65abc0926c04053f3ba5ea8f30f7
Rob Landley <rob@landley.net>
2013-03-11 22:23:46 -0500

    Compile time probe to fish O_NOFOLLOW out of linux headers when fcntl doesn't conform to posix-2008.

--- lib/portability.h
+++ lib/portability.h
@@ -145,3 +145,5 @@ int clearenv(void);
 ssize_t getdelim(char **lineptr, size_t *n, int delim, FILE *stream);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 #endif
+
+#include "generated/portability.h"
