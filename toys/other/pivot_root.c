50b2625145f4919c3752c9e974f88a5666cac476
Rob Landley <rob@landley.net>
2013-06-18 18:13:06 -0500

    Add pivot_root.

---
+++ toys/other/pivot_root.c
@@ -0,0 +1,31 @@
+/* pivot_root.c - edit system mount tree
+ *
+ * Copyright 2012 Rob Landley <rob@landley.net>
+
+USE_PIVOT_ROOT(NEWTOY(pivot_root, "<2>2", TOYFLAG_USR|TOYFLAG_BIN))
+
+config PIVOT_ROOT
+  bool "pivot_root"
+  default y
+  help
+    usage: pivot_root OLD NEW
+
+    Swap OLD and NEW filesystems (as if by simultaneous mount --move), and
+    move all processes with chdir or chroot under OLD into NEW (including
+    kernel threads) so OLD may be unmounted.
+
+    The directory NEW must exist under OLD. This doesn't work on initramfs,
+    which can't be moved (about the same way PID 1 can't be killed; see
+    switch_root instead).
+*/
+
+#define FOR_pivot_root
+#include "toys.h"
+
+#include <linux/unistd.h>
+
+void pivot_root_main(void)
+{
+  if (syscall(__NR_pivot_root, toys.optargs[0], toys.optargs[1]))
+    perror_exit("'%s' -> '%s'", toys.optargs[0], toys.optargs[1]);
+}
