2b0b548381c2872cc1d6ea0603632c0ee7cad211
Rob Landley <rob@landley.net>
2014-04-01 18:20:00 -0500

    New toy fsfreeze, from Isaac Dunham, plus minor cleanups.

---
+++ toys/other/fsfreeze.c
@@ -0,0 +1,30 @@
+/* fsfreeze.c - freeze or thaw filesystem
+ *
+ * No standard.
+
+USE_FSFREEZE(NEWTOY(fsfreeze, "<1>1f|u|[!fu]", TOYFLAG_USR|TOYFLAG_SBIN))
+
+config FSFREEZE
+  bool "fsfreeze"
+  default y
+  help
+    usage: fsfreeze {-f | -u} MOUNTPOINT
+
+    Freeze or unfreeze a filesystem.
+
+    -f	freeze
+    -u	unfreeze
+*/
+
+#define FOR_fsfreeze
+#include "toys.h"
+#include <linux/fs.h>
+
+void fsfreeze_main(void)
+{
+  int fd = xopen(*toys.optargs, O_RDONLY); 
+  long p = 1;
+
+  xioctl(fd, (toys.optflags & FLAG_f) ? FIFREEZE : FITHAW, &p);
+  xclose(fd);
+}
