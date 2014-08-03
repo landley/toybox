44b89867e0bfc085083e17657697af74e273a534
Rob Landley <rob@landley.net>
2014-08-02 19:54:38 -0500

    Missed checking in dirtree_recurse() change (now needs specific DIRTREE_SYMFOLLOW value, not just true/false).

--- toys/posix/ls.c
+++ toys/posix/ls.c
@@ -276,7 +276,7 @@ static void listfiles(int dirfd, struct dirtree *indir)
   } else {
     // Read directory contents. We dup() the fd because this will close it.
     indir->data = dup(dirfd);
-    dirtree_recurse(indir, filter, (flags&FLAG_L));
+    dirtree_recurse(indir, filter, (flags&FLAG_L) ? DIRTREE_SYMFOLLOW : 0);
   }
 
   // Copy linked list to array and sort it. Directories go in array because
