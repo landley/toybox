be3a48c269cfa4cf35e0bb31155553a8d3c6d6c1
Rob Landley <rob@landley.net>
2014-08-12 18:05:55 -0500

    Enable a command's sub-options in single builds.

--- scripts/single.sh
+++ scripts/single.sh
@@ -15,6 +15,7 @@ USET="is not set"
 make allnoconfig > /dev/null &&
 sed -i -e "s/\(CONFIG_TOYBOX\)=y/# \1 $USET/" \
        -e "s/# \(CONFIG_$NAME\) $USET/\1=y/"  \
+       -e "s/# \(CONFIG_${NAME}_.*\) $USET/\1=y/" \
        -e "s/# \(CONFIG_TOYBOX_HELP.*\) $USET/\1=y/" \
        -e "s/# \(CONFIG_TOYBOX_I18N\) $USET/\1=y/" \
        -e "s/# \(CONFIG_TOYBOX_FLOAT\) $USET/\1=y/" \
