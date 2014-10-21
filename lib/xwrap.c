977e48e1626b3e3f1f1f9b14f05ffc11e252455f
Rob Landley <rob@landley.net>
2014-10-20 19:52:29 -0500

    Add TOYBOX_NORECURSE so xexec() won't make internal function calls.

--- lib/xwrap.c
+++ lib/xwrap.c
@@ -137,7 +137,7 @@ void xexec_optargs(int skip)
 // with a path isn't a builtin, so /bin/sh won't match the builtin sh.
 void xexec(char **argv)
 {
-  if (CFG_TOYBOX) toy_exec(argv);
+  if (CFG_TOYBOX && !CFG_TOYBOX_NORECURSE) toy_exec(argv);
   execvp(argv[0], argv);
 
   perror_exit("exec %s", argv[0]);
