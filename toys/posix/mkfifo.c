72f2a25ee23fd30c8504965e5d6d9e1a2046be89
Michal Eljasiewicz <m.eljasiewic@samsung.com>
2014-10-24 14:06:27 +0200

    mkfifo: Add -Z (Smack) option
    Change-Id: I7290f13c320b673f6a521491a037d298c754a8e9
    Signed-off-by: Michal Eljasiewicz <m.eljasiewic@samsung.com>
    Signed-off-by: José Bollo <jose.bollo@open.eurogiciel.org>

--- toys/posix/mkfifo.c
+++ toys/posix/mkfifo.c
@@ -4,7 +4,7 @@
  *
  * See http://opengroup.org/onlinepubs/9699919799/utilities/mkfifo.html
 
-USE_MKFIFO(NEWTOY(mkfifo, "<1m:", TOYFLAG_USR|TOYFLAG_BIN))
+USE_MKFIFO(NEWTOY(mkfifo, "<1m:"USE_MKFIFO_SMACK("Z:"), TOYFLAG_BIN))
 
 config MKFIFO
   bool "mkfifo"
@@ -13,12 +13,23 @@ config MKFIFO
     usage: mkfifo [fifo_name...]
 
     Create FIFOs (named pipes).
+
+config MKFIFO_SMACK
+  bool
+  default y
+  depends on MKFIFO && TOYBOX_SMACK
+  help
+    usage: mkfifo [-Z context]
+
+    -Z	Set security 'context' to created file
+
 */
 
 #define FOR_mkfifo
 #include "toys.h"
 
 GLOBALS(
+  char *arg_context;
   char *m_string;
   mode_t mode;
 )
@@ -30,6 +41,17 @@ void mkfifo_main(void)
   TT.mode = 0666;
   if (toys.optflags & FLAG_m) TT.mode = string_to_mode(TT.m_string, 0);
 
-  for (s = toys.optargs; *s; s++)
-    if (mknod(*s, S_IFIFO | TT.mode, 0) < 0) perror_msg("%s", *s);
+  for (s = toys.optargs; *s; s++) {
+    if (mknod(*s, S_IFIFO | TT.mode, 0) < 0) {
+      perror_msg("%s", *s);
+    }
+    if (CFG_MKFIFO_SMACK) {
+      if (toys.optflags & FLAG_Z) {
+        if (smack_set_label_for_path(*s, XATTR_NAME_SMACK, 0, TT.arg_context) < 0) {
+          unlink(*s);
+          error_exit("Unable to create fifo '%s' with '%s' as context.", *s, TT.arg_context);
+        }
+      }
+    }
+  }
 }
