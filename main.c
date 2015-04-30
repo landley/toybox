e7c09548b9c6188857260064c8ceba03d850c4b7
Rob Landley <rob@landley.net>
2015-04-30 15:11:34 -0500

    Add default sigpipe handler for android (as suggested by Elliott Hughes).

--- main.c
+++ main.c
@@ -183,8 +183,17 @@ void toybox_main(void)
   xputc('\n');
 }
 
+static void shutup_sigpipe(int i)
+{
+  // exit success from sigpipe to mollify overzealous crash reporting.
+  _exit(0);
+}
+
 int main(int argc, char *argv[])
 {
+  if (CFG_TOYBOX_ON_ANDROID) signal(SIGPIPE, shutup_sigpipe);
+  else signal(SIGPIPE, SIG_IGN);
+
   if (CFG_TOYBOX) {
     // Trim path off of command name
     *argv = basename(*argv);
