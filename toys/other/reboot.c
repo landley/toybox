a12eec740dc6d0c533fb7d83a0a54de6d3abb17c
Isaac Dunham <ibid.ag@gmail.com>
2015-08-05 21:33:43 -0500

    reboot: signal init, add -f option to call reboot(); init: use SIGINT, use xsignal()
    Now that oneit supports the same signals as our "sysv"-init for shutdown,
    make reboot et al. signal pid 1 with the appropriate signal.
    Of these signals, only SIGINT works with sysvinit 2.88, causing a reboot.
    The others are only supported in Busybox init.
    
    Also, make init accept SIGINT and use xsignal().

--- toys/other/reboot.c
+++ toys/other/reboot.c
@@ -2,7 +2,7 @@
  *
  * Copyright 2013 Elie De Brauwer <eliedebrauwer@gmail.com>
 
-USE_REBOOT(NEWTOY(reboot, "n", TOYFLAG_SBIN|TOYFLAG_NEEDROOT))
+USE_REBOOT(NEWTOY(reboot, "fn", TOYFLAG_SBIN|TOYFLAG_NEEDROOT))
 USE_REBOOT(OLDTOY(halt, reboot, TOYFLAG_SBIN|TOYFLAG_NEEDROOT))
 USE_REBOOT(OLDTOY(poweroff, reboot, TOYFLAG_SBIN|TOYFLAG_NEEDROOT))
 
@@ -10,10 +10,11 @@ config REBOOT
   bool "reboot"
   default y
   help
-    usage: reboot/halt/poweroff [-n]
+    usage: reboot/halt/poweroff [-fn]
 
     Restart, halt or powerdown the system.
 
+    -f	Don't signal init
     -n	Don't sync before stopping the system.
 */
 
@@ -24,8 +25,12 @@ config REBOOT
 void reboot_main(void)
 {
   int types[] = {RB_AUTOBOOT, RB_HALT_SYSTEM, RB_POWER_OFF};
+  int sigs[] = {SIGINT, SIGUSR1, SIGUSR2};
 
   if (!(toys.optflags & FLAG_n)) sync();
 
-  toys.exitval = reboot(types[stridx("hp", *toys.which->name)+1]);
+  if (toys.optflags & FLAG_f)
+    toys.exitval = reboot(types[stridx("hp", *toys.which->name)+1]);
+  else
+    toys.exitval = kill(1, sigs[stridx("hp", *toys.which->name)+1]);
 }
--- toys/pending/init.c
+++ toys/pending/init.c
@@ -348,6 +348,7 @@ static void halt_poweroff_reboot_handler(int sig_no)
       reboot_magic_no=RB_POWER_OFF;
       break;
     case SIGTERM:  
+    case SIGINT:
       error_msg("Requesting system reboot");
       reboot_magic_no=RB_AUTOBOOT;
       break;
@@ -415,7 +416,7 @@ static void pause_handler(int sig_no)
 
   errno_backup = errno;
   signal_backup = caught_signal;
-  signal(SIGCONT, catch_signal);
+  xsignal(SIGCONT, catch_signal);
 
   while(1) {
     if (caught_signal == SIGCONT) break;
@@ -460,10 +461,11 @@ void init_main(void)
   putenv("USER=root");
 
   inittab_parsing();  
-  signal(SIGUSR1, halt_poweroff_reboot_handler);//halt
-  signal(SIGUSR2, halt_poweroff_reboot_handler);//poweroff
-  signal(SIGTERM, halt_poweroff_reboot_handler);//reboot
-  signal(SIGQUIT, restart_init_handler);//restart init
+  xsignal(SIGUSR1, halt_poweroff_reboot_handler);//halt
+  xsignal(SIGUSR2, halt_poweroff_reboot_handler);//poweroff
+  xsignal(SIGTERM, halt_poweroff_reboot_handler);//reboot
+  xsignal(SIGINT, halt_poweroff_reboot_handler);//reboot
+  xsignal(SIGQUIT, restart_init_handler);//restart init
   memset(&sig_act, 0, sizeof(sig_act));
   sigfillset(&sig_act.sa_mask);
   sigdelset(&sig_act.sa_mask, SIGCONT);
