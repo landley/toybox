4e49e2cca731f2e1dd01ebc5163beb71e0d640ec
Rob Landley <rob@landley.net>
2015-08-17 23:23:29 -0500

    Test for human_readable()

---
+++ toys/example/test_human_readable.c
@@ -0,0 +1,23 @@
+/* test_human_readable.c - Expose lib/lib.c human_readable() for testing.
+ *
+ * Copyright 2015 Rob Landley <rob@landley.net>
+
+USE_TEST_HUMAN_READABLE(NEWTOY(test_human_readable, "<1>1ibs", 0))
+
+config TEST_HUMAN_READABLE
+  bool "test_human_readable"
+  default n
+  help
+    usage: test_human_readable [-sbi] NUMBER
+*/
+
+#define FOR_test_human_readable
+#include "toys.h"
+
+void test_human_readable_main(void)
+{
+  char *c;
+
+  human_readable(toybuf, strtoll(*toys.optargs, &c, 0), toys.optflags);
+  printf("%s\n", toybuf);
+}
