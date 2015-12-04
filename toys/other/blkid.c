2a3ac1459f857f17e1c273c49a8df000b14fc8db
Rob Landley <rob@landley.net>
2015-12-04 16:17:03 -0600

    Tom Marshall reported that blkid was handling ext2 wrong.
    Even though ext2 has a comment that it has to be at the start, I added swap
    to the start of the array (oops). The test suite was also wrong (it was
    matching the _incorrect_ output).

--- toys/other/blkid.c
+++ toys/other/blkid.c
@@ -34,8 +34,8 @@ struct fstype {
 };
 
 static const struct fstype fstypes[] = {
-  {"swap", 0x4341505350415753LL, 8, 4086, 1036, 15, 1052},
   {"ext2", 0xEF53, 2, 1080, 1128, 16, 1144}, // keep this first for ext3/4 check
+  {"swap", 0x4341505350415753LL, 8, 4086, 1036, 15, 1052},
   // NTFS label actually 8/16 0x4d80 but horrible: 16 bit wide characters via
   // codepage, something called a uuid that's only 8 bytes long...
   {"ntfs", 0x5346544e, 4, 3, 0x48+(8<<24), 0, 0},
