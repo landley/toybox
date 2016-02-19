1f4c82a333f7b08d18109343024dc56fedac2641
Lipi C.H. Lee <lipisoft@gmail.com>
2016-02-19 19:32:37 +0900

    wget initial submit version

--- toys/pending/wget.c
+++ toys/pending/wget.c
@@ -10,9 +10,12 @@ config WGET
   default n
   help
     usage: wget -f filename URL
+    -f filename: specify the filename to be saved
+    URL: HTTP uniform resource location and only HTTP, not HTTPS
 
-    Simple downloader to get the resource file in HTTP server
-
+    examples:
+      wget -f index.html http://www.example.com
+      wget -f sample.jpg http://www.example.com:8080/sample.jpg
 */
 
 #define FOR_wget
@@ -31,23 +34,18 @@ struct httpinfo {
   char path[PATH_LEN];
 };
 
-// get the hostname's size in URL
-static unsigned int get_hnsz(char *url) {
+// extract hostname from url
+static unsigned int get_hn(char *url, char *hn) {
   unsigned int i;
 
-  for (i=0; url[i] != '\0' && url[i] != ':' && url[i] != '/'; i++)
-    ;
+  for (i = 0; url[i] != '\0' && url[i] != ':' && url[i] != '/'; i++) {
+    if(i >= HN_LEN)
+      error_exit("The hostname's length is lower than %d.", HN_LEN);
+    hn[i] = url[i];
+  }
+  hn[i] = '\0';
 
   return i;
-}
-
-// extract hostname from url
-static void get_hn(char *url, char *hn) {
-  int i;
-
-  for (i = 0; url[i] != '\0' && url[i] != ':' && url[i] != '/'; i++)
-    hn[i] = url[i];
-  hn[i] = '\0';
 }
 
 // extract port number
@@ -67,9 +65,7 @@ static void get_info(struct httpinfo *hi, char *url) {
   unsigned int i = 7, len;
 
   if (strncmp(url, "http://", i)) error_exit("Only HTTP can be supported.");
-  if ((len = get_hnsz(url+i)) >= HN_LEN)
-    error_exit("The hostname's length is lower than %d.", HN_LEN);
-  get_hn(url+i, hi->hostname);
+  len = get_hn(url+i, hi->hostname);
   i += len;
 
   // get port if exists
@@ -89,7 +85,7 @@ static void get_info(struct httpinfo *hi, char *url) {
 // connect to any IPv4 or IPv6 server
 static int conn_svr(const char *hostname, const char *port) {
   struct addrinfo hints, *result, *rp;
-  int sock_fd;
+  int sock;
 
   memset(&hints, 0, sizeof(struct addrinfo));
   hints.ai_family = AF_UNSPEC;
@@ -102,20 +98,20 @@ static int conn_svr(const char *hostname, const char *port) {
 
   // try all address list(IPv4 or IPv6) until success
   for (rp = result; rp; rp = rp->ai_next) {
-    if ((sock_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol))
+    if ((sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol))
         == -1) {
       perror_msg("Socket Error");
       continue;
     }
-    if (connect(sock_fd, rp->ai_addr, rp->ai_addrlen) != -1)
+    if (connect(sock, rp->ai_addr, rp->ai_addrlen) != -1)
       break; // succeed in connecting to any server IP 
     else perror_msg("Connect Error");
-    close(sock_fd);
+    close(sock);
   }
   freeaddrinfo(result);
   if(!rp) error_exit("Can not connect to HTTP server");
 
-  return sock_fd;
+  return sock;
 }
 
 // make HTTP request header field
@@ -133,34 +129,35 @@ static void mk_rq(char *path) {
   strcat(toybuf, " HTTP/1.1\r\n");
 }
 
-// get http response body address and its length
+// get http response body starting address and its length
 static char *get_body(size_t len, size_t *body_len) {
   unsigned int i;
 
-  for (i = 0; i < len; i++) {
-    if (toybuf[i] == '\r' && toybuf[i+1] == '\n' && toybuf[i+2] == '\r'
-        && toybuf[i+3]) break;
-  }
+  for (i = 0; i < len-4; i++)
+    if (!strncmp(toybuf+i, "\r\n\r\n", 4)) break;
+
   *body_len = len-i-4;
   return toybuf+i+4;
 }
 
 void wget_main(void)
 {
-  int sock_fd;
+  int sock;
   struct httpinfo hi;
   FILE *fp;
   size_t len, body_len;
-  char *body, *result, *rcode, *r_str, ua[18] = "toybox wget/", ver[6];
+  char *body, *result, *rc, *r_str, ua[18] = "toybox wget/", ver[6];
 
-  if (!(toys.optflags & FLAG_f)) help_exit("Filename should be needed.");
+  // TODO extract filename to be saved from URL
+  if (!(toys.optflags & FLAG_f))
+    help_exit("The filename to be saved should be needed.");
   if (fopen(TT.filename, "r"))
-    error_exit("The file you specified already exists.");
+    error_exit("The file(%s) you specified already exists.", TT.filename);
 
   if(!toys.optargs[0]) help_exit("The URL should be specified.");
   get_info(&hi, toys.optargs[0]);
 
-  sock_fd = conn_svr(hi.hostname, hi.port);
+  sock = conn_svr(hi.hostname, hi.port);
 
   // compose HTTP request
   mk_rq(hi.path);
@@ -173,25 +170,28 @@ void wget_main(void)
 
   // send the HTTP request
   len = strlen(toybuf);
-  if (write(sock_fd, toybuf, len) != len) perror_exit("HTTP GET failed.");
+  if (write(sock, toybuf, len) != len) perror_exit("HTTP GET failed.");
 
   // read HTTP response
-  if ((len = read(sock_fd, toybuf, 4096)) == -1)
+  if ((len = read(sock, toybuf, 4096)) == -1)
     perror_exit("HTTP response failed.");
   if (!strstr(toybuf, "\r\n\r\n"))
     error_exit("HTTP response header is too long.");
   body = get_body(len, &body_len);
   result = strtok(toybuf, "\r");
   strtok(result, " ");
-  rcode = strtok(NULL, " ");
+  rc = strtok(NULL, " ");
   r_str = strtok(NULL, " ");
-  if (strcmp(rcode, "200")) error_exit("HTTP response: %s(%s).", rcode, r_str);
+
+  // HTTP res code check
+  // TODO handle HTTP 302 Found(Redirection)
+  if (strcmp(rc, "200")) error_exit("HTTP response: %s(%s).", rc, r_str);
 
   if (!(fp = fopen(TT.filename, "w"))) perror_exit("File write error");
   if (fwrite(body, sizeof(char), body_len, fp) != body_len)
-    error_exit("File write is not successful.0");
-  while ((len = read(sock_fd, toybuf, 4096)) > 0)
+    error_exit("File write is not successful.");
+  while ((len = read(sock, toybuf, 4096)) > 0)
     if (fwrite(toybuf, sizeof(char), len, fp) != len)
-      error_exit("File write is not successful.1");
+      error_exit("File write is not successful.");
   if (fclose(fp) == EOF) perror_exit("File Close Error");
 }
