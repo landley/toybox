5913da6ea5c15960cef54f2c7f080123330aa72d
Rob Landley <rob@landley.net>
2016-10-20 22:29:10 -0500

    Close to cutting a release and I don't want to have setfattr in defconfig without getfattr, so I'm putting it back in pending for now.

--- toys/pending/setfattr.c
+++ toys/pending/setfattr.c
@@ -8,7 +8,7 @@ USE_SETFATTR(NEWTOY(setfattr, "hn:|v:x:|[!xv]", TOYFLAG_USR|TOYFLAG_BIN))
 
 config SETFATTR
   bool "setfattr"
-  default y
+  default n
   help
     usage: setfattr [-h] [-x|-n NAME] [-v VALUE] FILE...
 
