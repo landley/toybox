16c4357f0d0df0ce4f91d424cc57ce571ccf4e91
Rob Landley <rob@landley.net>
2018-03-26 17:36:33 -0500

    Rename demo_human_readable.c->demo_number.c and have it do atolx() too.

---
+++ toys/example/demo_number.c
@@ -0,0 +1,33 @@
+/* demo_number.c - Expose atolx() and human_readable() for testing.
+ *
+ * Copyright 2015 Rob Landley <rob@landley.net>
+
+USE_DEMO_NUMBER(NEWTOY(demo_number, "hdbs", TOYFLAG_BIN))
+
+config DEMO_NUMBER
+  bool "demo_number"
+  default n
+  help
+    usage: demo_number [-hsbi] NUMBER...
+
+    -b	Use "B" for single byte units (HR_B)
+    -d	Decimal units
+    -h	human readable
+    -s	Space between number and units (HR_SPACE)
+*/
+
+#include "toys.h"
+
+void demo_number_main(void)
+{
+  char **arg;
+
+  for (arg = toys.optargs; *arg; arg++) {
+    long long ll = atolx(*arg);
+
+    if (toys.optflags) {
+      human_readable(toybuf, ll, toys.optflags);
+      xputs(toybuf);
+    } else printf("%lld\n", ll);
+  }
+}
