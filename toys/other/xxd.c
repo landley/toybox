8db5d5bef242d1486cf78dded8a5b44e069e944a
Zach van Rijn <me@zv.io>
2018-05-06 17:49:27 -0400

    Fix bug in 'xxd' causing incorrect translation for upper-case characters.

--- toys/other/xxd.c
+++ toys/other/xxd.c
@@ -103,7 +103,7 @@ static int dehex(char ch)
 {
   if (ch >= '0' && ch <= '9') return ch - '0';
   if (ch >= 'a' && ch <= 'f') return ch - 'a' + 10;
-  if (ch >= 'A' && ch <= 'F') return ch - 'a' + 10;
+  if (ch >= 'A' && ch <= 'F') return ch - 'A' + 10;
   return (ch == '\n') ? -2 : -1;
 }
 
