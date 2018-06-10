324c7812c7df545bbbe44bdf485225ad69a98801
Francesco Valla <valla.francesco@gmail.com>
2018-06-10 17:07:25 +0200

    toysh: add exec nonfork command

--- toys/pending/sh.c
+++ toys/pending/sh.c
@@ -38,6 +38,7 @@
  * TODO: test exit from "trap EXIT" doesn't recurse
 
 USE_SH(NEWTOY(cd, NULL, TOYFLAG_NOFORK))
+USE_SH(NEWTOY(exec, NULL, TOYFLAG_NOFORK))
 USE_SH(NEWTOY(exit, NULL, TOYFLAG_NOFORK))
 
 USE_SH(NEWTOY(sh, "c:i", TOYFLAG_BIN))
@@ -69,6 +70,15 @@ config CD
 
     -P	Physical path: resolve symlinks in path
     -L	Local path: .. trims directories off $PWD (default)
+
+config EXEC
+  bool
+  default n
+  depends on SH
+  help
+    usage: exec command [arguments]
+
+    Replace the shell process with command.
 
 config EXIT
   bool
@@ -113,6 +123,12 @@ void cd_main(void)
   char *dest = *toys.optargs ? *toys.optargs : getenv("HOME");
 
   xchdir(dest ? dest : "/");
+}
+
+void exec_main(void)
+{
+  if (*toys.optargs)
+    xexec(toys.optargs);
 }
 
 void exit_main(void)
