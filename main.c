19f7ad4949e12fa667b448b01a5b703ac6d926de
Rob Landley <rob@landley.net>
2018-09-16 14:17:09 -0500

    It should never matter on Linux (where the top half of virtual address space is reserved for the kernel), but technically a pointer is unsigned long.

--- main.c
+++ main.c
@@ -156,8 +156,8 @@ void toy_exec_which(struct toy_list *which, char *argv[])
 
   // Compiler writers have decided subtracting char * is undefined behavior,
   // so convert to integers. (LP64 says sizeof(long)==sizeof(pointer).)
-  if (!CFG_TOYBOX_NORECURSE)
-    if (toys.stacktop && labs((long)toys.stacktop-(long)&which)>6000) return;
+  if (!CFG_TOYBOX_NORECURSE && toys.stacktop)
+    if (labs((unsigned long)toys.stacktop-(unsigned long)&which)>6000) return;
 
   // Return if we need to re-exec to acquire root via suid bit.
   if (toys.which && (which->flags&TOYFLAG_ROOTONLY) && toys.wasroot) return;
