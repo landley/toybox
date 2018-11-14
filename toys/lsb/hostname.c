7d26f1071d17dc2980280008fbbcd8a6a376d387
Elliott Hughes <enh@google.com>
2018-11-14 15:16:29 -0800

    hostname: add -s/-f and -d.
    At least the AOSP build doesn't try to _set_ the hostname...

--- toys/lsb/hostname.c
+++ toys/lsb/hostname.c
@@ -4,48 +4,60 @@
  *
  * http://refspecs.linuxfoundation.org/LSB_4.1.0/LSB-Core-generic/LSB-Core-generic/hostname.html
 
-USE_HOSTNAME(NEWTOY(hostname, "bF:", TOYFLAG_BIN))
+USE_HOSTNAME(NEWTOY(hostname, ">1bdsfF:[!bdsf]", TOYFLAG_BIN))
 
 config HOSTNAME
   bool "hostname"
   default y
   help
-    usage: hostname [-b] [-F FILENAME] [newname]
+    usage: hostname [-bsf] [-F FILENAME] [newname]
 
-    Get/Set the current hostname
+    Get/set the current hostname.
 
-    -b	Set hostname to 'localhost' if otherwise unset
-    -F	Set hostname to contents of FILENAME
+    -b	set hostname to 'localhost' if otherwise unset
+    -d	show DNS domain name (no host)
+    -f	show fully-qualified name (host+domain, FQDN)
+    -F	set hostname to contents of FILENAME
+    -s	show short host name (no domain)
 */
 
 #define FOR_hostname
 #include "toys.h"
 
 GLOBALS(
-  char *fname;
+  char *F;
 )
 
 void hostname_main(void)
 {
-  char *hostname = *toys.optargs;
+  char *hostname = *toys.optargs, *dot;
+  struct hostent *h;
 
-  if (TT.fname && (hostname = xreadfile(TT.fname, 0, 0))) {
+  if (TT.F && (hostname = xreadfile(TT.F, 0, 0))) {
     if (!*chomp(hostname)) {
       if (CFG_TOYBOX_FREE) free(hostname);
-      if (!(toys.optflags&FLAG_b)) error_exit("empty '%s'", TT.fname);
+      if (!(toys.optflags&FLAG_b)) error_exit("empty '%s'", TT.F);
       hostname = 0;
     }
   }
 
+  // Implement -b.
   if (!hostname && (toys.optflags&FLAG_b))
     if (gethostname(toybuf, sizeof(toybuf)-1) || !*toybuf)
       hostname = "localhost";
 
+  // Setting?
   if (hostname) {
     if (sethostname(hostname, strlen(hostname)))
       perror_exit("set '%s'", hostname);
-  } else {
-    if (gethostname(toybuf, sizeof(toybuf)-1)) perror_exit("gethostname");
-    xputs(toybuf);
+    return;
   }
+
+  // Get the hostname.
+  if (gethostname(toybuf, sizeof(toybuf)-1)) perror_exit("gethostname");
+  if (!(h = gethostbyname(toybuf))) perror_exit("gethostbyname");
+  snprintf(toybuf, sizeof(toybuf), "%s", h->h_name);
+  dot = strchr(toybuf, '.');
+  if (toys.optflags&FLAG_s) *dot = '\0';
+  xputs(toys.optflags&FLAG_d ? dot+1 : toybuf);
 }
