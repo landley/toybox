66050d1a023b4fde1ff0e30a31f2b09437605f3d
Rob Landley <rob@landley.net>
2019-06-20 22:19:13 -0500

    The O_PATH breaks stuff, have to work around Android's selinux rules for / some other way.

--- lib/dirtree.c
+++ lib/dirtree.c
@@ -117,7 +117,7 @@ struct dirtree *dirtree_handle_callback(struct dirtree *new,
 
   if (S_ISDIR(new->st.st_mode) && (flags & (DIRTREE_RECURSE|DIRTREE_COMEAGAIN)))
     flags = dirtree_recurse(new, callback,
-      openat(dirtree_parentfd(new), new->name, O_PATH|O_CLOEXEC), flags);
+      openat(dirtree_parentfd(new), new->name, O_CLOEXEC), flags);
 
   // If this had children, it was callback's job to free them already.
   if (!(flags & DIRTREE_SAVE)) {
