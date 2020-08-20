landley/toybox

commit 42303209f44a335025b9cd1dbe5dd2f3069f2e99

If we fflush(stdout) first we can just write to filehandle.

lib/xwrap.c
@@ -161,12 +161,8 @@ void xputsl(char *s, int len)
 {
   int out;
 
-  while (len != (out = fwrite(s, 1, len, stdout))) {
-    if (out<1) perror_exit("write");
-    len -= out;
-    s += out;
-  }
   xflush(0);
+  xwrite(0, s, len);
 }
 
 // xputs with no newline

commit 0edcf02d769d07eae662df4e044177f984c343ef

xputsl: write to stdout, not stdin!
This is why the tests have been failing since
42303209f44a335025b9cd1dbe5dd2f3069f2e99.

lib/xwrap.c
@@ -160,7 +160,7 @@ void xprintf(char *format, ...)
 void xputsl(char *s, int len)
 {
   xflush(0);
-  xwrite(0, s, len);
+  xwrite(1, s, len);
 }
 
 // xputs with no newline