cd4c2da20a24116117842060798002032c4f5bb0
Rob Landley <rob@landley.net>
2022-04-11 15:45:36 -0500

    Update man page URL in hello/skeleton examples, and fix hello to wait for PID 1. (rdinit=/bin/hello would kernel panic if pid 1 exits, and wait() returns "No child processes" error, so pause() instead.)

--- toys/example/hello.c
+++ toys/example/hello.c
@@ -5,7 +5,7 @@
  * See http://pubs.opengroup.org/onlinepubs/9699919799/utilities/
  * See http://refspecs.linuxfoundation.org/LSB_4.1.0/LSB-Core-generic/LSB-Core-generic/cmdbehav.html
  * See https://www.ietf.org/rfc/rfc3.txt
- * See http://man7.org/linux/man-pages/dir_section_1.html
+ * see https://man7.org/linux/man-pages/man1/intro.1.html
 
 USE_HELLO(NEWTOY(hello, 0, TOYFLAG_USR|TOYFLAG_BIN))
 
@@ -33,5 +33,5 @@ void hello_main(void)
   xprintf("Hello world\n");
 
   // Avoid kernel panic if run as init.
-  if (getpid() == 1) wait(&TT.unused);
+  if (getpid() == 1) getchar();
 }
