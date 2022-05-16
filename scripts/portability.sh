0961ea2954afddce9a81e02004d8b50fa5fd2ebb
Rob Landley <rob@landley.net>
2022-05-16 15:45:43 -0500

    Move the mac-only option into the mac-only section.

--- scripts/portability.sh
+++ scripts/portability.sh
@@ -16,6 +16,7 @@ fi
 # Tell linker to do dead code elimination at function level
 if [ "$(uname)" == "Darwin" ]
 then
+  CFLAGS+=" -Wno-deprecated-declarations"
   : ${LDOPTIMIZE:=-Wl,-dead_strip} ${STRIP:=strip}
 else
   : ${LDOPTIMIZE:=-Wl,--gc-sections -Wl,--as-needed} ${STRIP:=strip -s -R .note* -R .comment}
