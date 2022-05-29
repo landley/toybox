6d4847934fc0fe47a3254ce6c0396d197a780cf4
Rob Landley <rob@landley.net>
2022-05-29 06:19:30 -0500

    Don't segfault if xabspath() returns null (spotted by Taolaw).

--- toys/net/httpd.c
+++ toys/net/httpd.c
@@ -83,7 +83,7 @@ char *mime(char *file)
 static int isunder(char *file, char *dir)
 {
   char *s1 = xabspath(dir, ABS_FILE), *s2 = xabspath(file, 0), *ss = s2;
-  int rc = strstart(&ss, s2) && (!*ss || *ss=='/' || ss[-1]=='/');
+  int rc = s1 && s2 && strstart(&ss, s1) && (!*ss || *ss=='/' || ss[-1]=='/');
 
   free(s2);
   free(s1);
