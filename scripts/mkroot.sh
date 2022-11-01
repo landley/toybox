0866cdd2a087b8e5858beafef5592ebb9f563e76
Rob Landley <rob@landley.net>
2022-11-01 18:33:36 -0500

    More specific sed to hit just the one config symbol.

--- scripts/mkroot.sh
+++ scripts/mkroot.sh
@@ -262,7 +262,7 @@ else
   pushd "$LINUX" && make distclean && popd &&
   cp -sfR "$LINUX" "$TEMP/linux" && pushd "$TEMP/linux" &&
   # Fix x86-64, sh2eb, and building with cc pointing to llvm or gcc
-  sed -Eis '/select HAVE_(STACK_VALIDATION|OBJTOOL)/d' arch/x86/Kconfig &&
+  sed -Eis '/select HAVE_(STACK_VALIDATION|OBJTOOL)[^_]/d' arch/x86/Kconfig &&
   sed -is 's/depends on !SMP/& || !MMU/' mm/Kconfig &&
   sed -i 's@=\(.*[ )]\)gcc$@:=\1$(shell $(CROSS_COMPILE)cc --version >/dev/null 2>\&1 \&\& echo cc || echo gcc)@' Makefile &&
 
