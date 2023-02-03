f0b4f9e5bb263ea63207d9355c87d02f9f04efed
Rob Landley <rob@landley.net>
2023-02-02 21:19:48 -0600

    Make head lseek backwards to return unused data from seekable inputs.

--- toys/posix/head.c
+++ toys/posix/head.c
@@ -34,7 +34,7 @@ GLOBALS(
 
 static void do_head(int fd, char *name)
 {
-  long i, len, lines=TT.n, bytes=TT.c;
+  long i = 0, len = 0, lines = TT.n, bytes = TT.c;
 
   if ((toys.optc > 1 && !FLAG(q)) || FLAG(v)) {
     // Print an extra newline for all but the first file
@@ -50,10 +50,13 @@ static void do_head(int fd, char *name)
     if (bytes) {
       i = bytes >= len ? len : bytes;
       bytes -= i;
-    } else for(i=0; i<len;) if (toybuf[i++] == '\n' && !--lines) break;
+    } else for(i = 0; i<len;) if (toybuf[i++] == '\n' && !--lines) break;
 
     xwrite(1, toybuf, i);
   }
+
+  // attempt to unget extra data
+  if (len>i) lseek(fd, i-len, SEEK_CUR);
 
   TT.file_no++;
 }
