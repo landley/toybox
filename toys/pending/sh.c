landley/toybox

commit 7fac232b4d25e51372ee22512c92e724ce97734e

Don't trigger ASAN leak detector.

toys/pending/sh.c
@@ -4394,7 +4394,8 @@ void cd_main(void)
 
 void exit_main(void)
 {
-  exit(*toys.optargs ? atoi(*toys.optargs) : 0);
+  toys.exitval = *toys.optargs ? atoi(*toys.optargs) : 0;
+  xexit();
 }
 
 // lib/args.c can't +prefix & "+o history" needs space so parse cmdline here

commit 176abf43dd8b41e66256ad7bb78d7a4df974a978

Fix sh "exit" command to actually exit (broken in commit 7fac232b4d25).

toys/pending/sh.c
@@ -4395,6 +4395,8 @@ void cd_main(void)
 void exit_main(void)
 {
   toys.exitval = *toys.optargs ? atoi(*toys.optargs) : 0;
+  toys.rebound = 0;
+  // TODO trap EXIT, sigatexit
   xexit();
 }
 