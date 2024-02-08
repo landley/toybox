8fbff9dc2e8f3f444437c82835f22ba5d6a73367
Rob Landley <rob@landley.net>
2024-02-08 05:43:52 -0600

    Add wc -L and tests.

--- toys/posix/wc.c
+++ toys/posix/wc.c
@@ -4,16 +4,17 @@
  *
  * See http://opengroup.org/onlinepubs/9699919799/utilities/wc.html
 
-USE_WC(NEWTOY(wc, "mcwl", TOYFLAG_USR|TOYFLAG_BIN))
+USE_WC(NEWTOY(wc, "Lcmwl", TOYFLAG_USR|TOYFLAG_BIN))
 
 config WC
   bool "wc"
   default y
   help
-    usage: wc [-lwcm] [FILE...]
+    usage: wc [-Llwcm] [FILE...]
 
     Count lines, words, and characters in input.
 
+    -L	Show max line length
     -l	Show lines
     -w	Show words
     -c	Show bytes
@@ -28,7 +29,7 @@ config WC
 #include "toys.h"
 
 GLOBALS(
-  unsigned long totals[4];
+  unsigned long totals[5];
 )
 
 static void show_lengths(unsigned long *lengths, char *name)
@@ -45,12 +46,13 @@ static void show_lengths(unsigned long *lengths, char *name)
   if (!(!toys.optc && !(toys.optflags & (toys.optflags-1))) && toys.optc!=1)
     space = 7;
 
-  for (i = 0; i<4; i++) {
+  for (i = 0; i<ARRAY_LEN(TT.totals); i++) {
     if (toys.optflags&(1<<i)) {
       printf(" %*ld"+first, space, lengths[i]);
       first = 0;
     }
-    TT.totals[i] += lengths[i];
+    if (i==4) TT.totals[i] = maxof(TT.totals[i], lengths[i]);
+    else TT.totals[i] += lengths[i];
   }
   if (*toys.optargs) printf(" %s", name);
   xputc('\n');
@@ -59,15 +61,15 @@ static void show_lengths(unsigned long *lengths, char *name)
 static void do_wc(int fd, char *name)
 {
   int len = 0, clen = 1, space = 0;
-  unsigned long word = 0, lengths[] = {0,0,0,0};
+  unsigned long word = 0, lengths[ARRAY_LEN(TT.totals)] = {0}, line = 0;
 
-  // Speed up common case: wc -c normalfile is file length.
+  // fast path: wc -c normalfile is file length.
   if (toys.optflags == FLAG_c) {
     struct stat st;
 
     // On Linux, files in /proc often report their size as 0.
     if (!fstat(fd, &st) && S_ISREG(st.st_mode) && st.st_size) {
-      lengths[2] = st.st_size;
+      lengths[3] = st.st_size;
       goto show;
     }
   }
@@ -82,8 +84,8 @@ static void do_wc(int fd, char *name)
 
     for (pos = 0; pos<len; pos++) {
       if (toybuf[pos]=='\n') lengths[0]++;
-      lengths[2]++;
-      if (FLAG(m)) {
+      lengths[3]++;
+      if (FLAG(m)||FLAG(L)) {
         // If we've consumed next wide char
         if (--clen<1) {
           // next wide size, don't count invalid, fetch more data if necessary
@@ -91,7 +93,14 @@ static void do_wc(int fd, char *name)
           if (clen == -1) continue;
           if (clen == -2 && !done) break;
 
-          lengths[3]++;
+          lengths[2]++;
+          line += maxof(wcwidth(wchar), 0);
+          if (wchar=='\t') line += 8-(line&7);
+          else if (wchar=='\n' || wchar=='\r') {
+            if (line>lengths[4]) lengths[4] = line;
+            line = 0;
+          }
+
           space = iswspace(wchar);
         }
       } else space = isspace(toybuf[pos]);
@@ -106,6 +115,7 @@ static void do_wc(int fd, char *name)
     if (pos != len) memmove(toybuf, toybuf+pos, len-pos);
     len -= pos;
   }
+  if (line>lengths[4]) lengths[4] = line;
 
 show:
   show_lengths(lengths, name);
