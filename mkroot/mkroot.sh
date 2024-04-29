6ab0eb54d53607bdc12b7e01af716b66cc102375
Rob Landley <rob@landley.net>
2024-04-29 12:07:04 -0500

    Don't rely on (skippable) airlock build to set mkroot VERSION.

--- mkroot/mkroot.sh
+++ mkroot/mkroot.sh
@@ -154,7 +154,9 @@ guest:x:500:500:guest:/home/guest:/bin/sh
 nobody:x:65534:65534:nobody:/proc/self:/dev/null
 EOF
 echo -e 'root:x:0:\nguest:x:500:\nnobody:x:65534:' > "$ROOT"/etc/group &&
-: ${VERSION:=$(toybox --version)} &&
+# Grab toybox version git or toys.h
+: ${VERSION:=$(git describe --tags --abbrev=12 2>/dev/null)} &&
+: ${VERSION:=$(sed -n 's/.*TOYBOX_VERSION "\([^"]*\)".*/\1/p' toys.h)} &&
 # Optional file, basically a comment
 echo $'NAME="mkroot"\nVERSION="'${VERSION#* }$'"\nHOME_URL="https://landley.net/toybox"' > "$ROOT"/etc/os-release || exit 1
 
